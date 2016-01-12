f50a23df5ecec84cc8a7ffe1e7e0e7e28546de0a
Raul Breje <Raul Breje>
2016-01-12 22:33:29 +0200

    Ethan is born

---
+++ Assets/Scripts/EthanController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EthanController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rbe;
+
+	void Start(){
+		rbe = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rbe.AddForce (movement * speed);
+	}
+
+}
